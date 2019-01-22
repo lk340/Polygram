249244094beafef0de7f1373218d12554286f28d
Loyd Kim <loyd.hkim@gmail.com>
2019-01-22 12:22:14 -0500

    Finished Users Migration, Installed Frontend Dependencies, Created Frontend Folder and Entry File

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :signed_in?
+  
+  # CSSSR
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out
+    session[:session_token] = current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user),permit(:username, :password, :email)
+  end
+end
