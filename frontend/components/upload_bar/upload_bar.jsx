ce9bbdb5704b685155062143e2e231b9a127ce59
Loyd Kim <loyd.hkim@gmail.com>
2019-01-25 12:30:45 -0500

    Finish image custom validation.

--- frontend/components/upload_bar/upload_bar.jsx
+++ frontend/components/upload_bar/upload_bar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// REMINDER: THIS IS FOR MOBILE VERSION (when width becomes small enough)!
+
 export default () => {
   return (
     <div className="upload-bar" >
