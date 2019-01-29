e65f6e0071a4afe1102b9d810142f20db3210813
Loyd Kim <loyd.hkim@gmail.com>
2019-01-29 14:40:09 -0500

    Finish post index css skeleton

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -4,11 +4,44 @@ export default class PostIndex extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { 
+      heartStatus: "heart-show", 
+      heart2Status: "heart-hide",
+      bookmarkStatus: "bookmark-show",
+      bookmark2Status: "bookmark-hide",
+    };
+
+    this.handleHeartClick = this.handleHeartClick.bind(this);
+    this.handleBookmarkClick = this.handleBookmarkClick.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
   }
   
   componentDidMount() {
     this.props.users();
     this.props.posts();
+  }
+
+  handleHeartClick() {
+    if (this.state.heartStatus === "heart-show" && this.state.heart2Status === "heart-hide") {
+      this.setState({ heartStatus: "heart-hide", heart2Status: "heart-show" });
+    }
+    else {
+      this.setState({ heartStatus: "heart-show", heart2Status: "heart-hide" });
+    }
+  }
+
+  handleBookmarkClick() {
+    if (this.state.bookmarkStatus === "bookmark-show" && this.state.bookmark2Status === "bookmark-hide") {
+      this.setState({ bookmarkStatus: "bookmark-hide", bookmark2Status: "bookmark-show" });
+    }
+    else {
+      this.setState({ bookmarkStatus: "bookmark-show", bookmark2Status: "bookmark-hide" });
+    }
+  }
+
+  handleCommentSubmit(event) {
+    event.preventDefault();
+    // insert thunk action creator for posting a comment into the back end
   }
   
   render() {
@@ -26,13 +59,14 @@ export default class PostIndex extends React.Component {
 
             <div className="post-actions">
               <div className="post-like-comment">
-                <div><i className="far fa-heart"></i></div>
-                <div className="heart-show"><i className="fas fa-heart red-heart"></i></div>
+                <div className={this.state.heartStatus} onClick={this.handleHeartClick}><i className="far fa-heart"></i></div>
+                <div className={this.state.heart2Status} onClick={this.handleHeartClick}><i className="fas fa-heart red-heart"></i></div>
                 <div><i className="far fa-comment"></i></div>
                 <div></div>
               </div>
 
-              <div><i className="far fa-bookmark"></i></div>
+              <div className={this.state.bookmarkStatus} onClick={this.handleBookmarkClick} ><i className="far fa-bookmark"></i></div>
+              <div className={this.state.bookmark2Status} onClick={this.handleBookmarkClick} ><i className="fas fa-bookmark"></i></div>
             </div>
 
             <div className="post-likes">insert # likes here</div>
@@ -40,6 +74,15 @@ export default class PostIndex extends React.Component {
             <p className="post-caption" key={`post-${ index }`}>
               <span className="post-username-span"> { this.props.allUsers[post.user_id].username } </span> { post.caption }
             </p>
+
+            <div className="post-index-comment-container">
+              <div className="post-index-comment">
+                <form className="post-index-comment-form" onSubmit={this.handleCommentSubmit}>
+                  <textarea placeholder="Add a comment..."></textarea>
+                </form>
+              </div>
+              <span>...</span>
+            </div>
           </div>
           )
       });
