45003945de84f7db441269c470cb39dae2afbe99
Loyd Kim <loyd.hkim@gmail.com>
2019-01-29 17:32:33 -0500

    Finish CSS likes, comment, bookmark in user_posts.jsx modal

--- frontend/components/user_profile/user_posts.jsx
+++ frontend/components/user_profile/user_posts.jsx
@@ -4,10 +4,20 @@ import Modal from 'react-modal';
 export default class UserPosts extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { modalOpen: false, photoURL: null, photoCaption: null };
+    this.state = {
+      modalOpen: false, 
+      photoURL: null, 
+      photoCaption: null,
+      heartStatus: "photo-show-heart",
+      heart2Status: "photo-hide-heart",
+      bookmarkStatus: "photo-show-bookmark",
+      bookmark2Status: "photo-hide-bookmark",
+    };
 
     this.handleClick = this.handleClick.bind(this);
     this.onModalClose = this.onModalClose.bind(this);
+    this.handleHeartClick = this.handleHeartClick.bind(this);
+    this.handleBookmarkClick = this.handleBookmarkClick.bind(this);
   }
   
   componentDidMount() {
@@ -23,6 +33,24 @@ export default class UserPosts extends React.Component {
 
   onModalClose() {
     this.setState({ modalOpen: false });
+  }
+
+  handleHeartClick() {
+    if (this.state.heartStatus === "photo-show-heart" && this.state.heart2Status === "photo-hide-heart") {
+      this.setState({ heartStatus: "photo-hide-heart", heart2Status: "photo-show-heart" });
+    }
+    else {
+      this.setState({ heartStatus: "photo-show-heart", heart2Status: "photo-hide-heart" });
+    }
+  }
+
+  handleBookmarkClick() {
+    if (this.state.bookmarkStatus === "photo-show-bookmark" && this.state.bookmark2Status === "photo-hide-bookmark") {
+      this.setState({ bookmarkStatus: "photo-hide-bookmark", bookmark2Status: "photo-show-bookmark" });
+    }
+    else {
+      this.setState({ bookmarkStatus: "photo-show-bookmark", bookmark2Status: "photo-hide-bookmark" });
+    }
   }
   
   render() {
@@ -83,7 +111,19 @@ export default class UserPosts extends React.Component {
               </div>
 
               <div className="photo-modal-data-bottom">
-                <div className="photo-modal-like-comment">like comment share favorite</div>
+                <div className="photo-modal-like-comment">
+                  <div className="user-posts-heart-icon">
+                    <div className={this.state.heartStatus} onClick={this.handleHeartClick}><i className="far fa-heart"></i></div>
+                    <div className={this.state.heart2Status} onClick={this.handleHeartClick}><i className="fas fa-heart red-heart"></i></div>
+
+                    <div><i class="far fa-comment"></i></div>
+                  </div>
+
+                  <div className="user-posts-bookmark-icon">
+                    <div className={this.state.bookmarkStatus} onClick={this.handleBookmarkClick}><i className="far fa-bookmark"></i></div>
+                    <div className={this.state.bookmark2Status} onClick={this.handleBookmarkClick}><i className="fas fa-bookmark"></i></div>
+                  </div>
+                </div>
                 <div className="photo-modal-number-likes">number likes</div>
                 <div className="photo-modal-post-date">when it was posted</div>
 
