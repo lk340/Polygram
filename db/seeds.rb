1701046cdc5b5fe4fbdb79599a1e6e538d1763f7
Loyd Kim <loyd.hkim@gmail.com>
2019-02-01 08:49:46 -0500

    Seed new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 User.destroy_all
 User.create(username: "demoUser", email: "demoUser@demo", password: "demodemo", name: "Demo User", biography: "I am a demo user!")
+User.create(username: "aaa", email: "aaa@aaa", password: "aaaaaa", name: "AAA aaa", biography: "AaaaaAAaAAaaAA aa aaAAa aA? AA.")
 
 # Post.destroy_all
 # Post.create(caption: "test")
