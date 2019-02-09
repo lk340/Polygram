7001aa8afa93b8c343e3daa14180dd5bc3edd8c0
Loyd Kim <loyd.hkim@gmail.com>
2019-02-09 11:51:15 -0500

    Add script to keep Heroku dyno awake at all times by sending a get request every 5 minutes

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter, Route } from 'react-router-dom';
+var http = require("http");
 
 import App from './app';
 import AppContainer from './app_container';
 
 export default ({ store }) => {
+  // Keeps Heroku dyno awake
+  setInterval(function() {
+      http.get("http://polygram-lk.herokuapp.com");
+  }, 300000); // every 5 minutes (300000)
+  
   return (
     <Provider store={ store }>
       <HashRouter>
