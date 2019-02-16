c0f98cc19f0e16e5a16a8bad0306a7edca9d170e
Loyd Kim <loyd.hkim@gmail.com>
2019-02-16 16:54:26 -0500

    Add 'delete?' CSS to user profile show page

--- frontend/components/user_profile/user_post_comments.jsx
+++ frontend/components/user_profile/user_post_comments.jsx
@@ -10,9 +10,12 @@ export default class UserPostComments extends React.Component {
       // comments: this.props.comments,
       comments: [],
       // postComments: null,
+      postDeleteSpan: "post-delete-span-hide",
     };
     
     this.handleCommentDelete = this.handleCommentDelete.bind(this);
+    this.handleCommentMouseOver = this.handleCommentMouseOver.bind(this);
+    this.handleCommentMouseLeave = this.handleCommentMouseLeave.bind(this);
   }
 
   componentDidMount() {
@@ -34,13 +37,28 @@ export default class UserPostComments extends React.Component {
     };
   }
 
+  handleCommentMouseOver() {
+    this.setState({ postDeleteSpan: "post-delete-span" });
+  }
+
+  handleCommentMouseLeave() {
+    this.setState({ postDeleteSpan: "post-delete-span-hide" });
+  }
+
   render() {
     let commentLis;
     if (this.state.comments) {
       commentLis = Object.values(this.props.allComments).map((commentObject, index) => {
         if (commentObject.post_id === this.props.postId) {
           // return <li key={`comment-${index}`}><b>{this.props.username}</b> <span className="comment-li" onClick={commentObject.user_id === this.props.currentUser.id ? this.handleCommentDelete(commentObject.id) : }>{commentObject.comment}</span></li>
-          return <li key={`comment-${index}`}><b>{<Link to={`/users/${commentObject.user_id}`} className="profile-link" onClick={this.props.modalClose}>{this.props.allUsers[commentObject.user_id].username}</Link>}</b> <span className="comment-li" onClick={commentObject.user_id === this.props.currentUser.id ? this.handleCommentDelete(commentObject.id) : this.handleCommentDelete(commentObject.id)}>{commentObject.comment}</span></li>
+          return <li key={`comment-${index}`}>
+            <b>{
+              <Link to={`/users/${commentObject.user_id}`} className="profile-link" onClick={this.props.modalClose}>
+                {this.props.allUsers[commentObject.user_id].username}
+              </Link>
+            }</b> 
+            &nbsp;<span className="comment-li" onMouseEnter={this.handleCommentMouseOver} onMouseLeave={this.handleCommentMouseLeave} onClick={commentObject.user_id === this.props.currentUser.id ? this.handleCommentDelete(commentObject.id) : this.handleCommentDelete(commentObject.id)}>{commentObject.comment}</span>
+            &nbsp;<span className={this.state.postDeleteSpan}>delete?</span></li>
         }
       });
     }
